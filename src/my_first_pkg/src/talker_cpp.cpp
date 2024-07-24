fe2b831dd857d8e49d33d8366988bfa4a3f1ef3a
Andre Murakami <murakkamy@gmail.com>
2024-07-24 04:39:41 -0300

    Treinamento ROS SEMEAR EESC - 3. ROS TOPICS - 8.Criando o nosso primeiro nó em C++ - versao1.0

--- src/my_first_pkg/src/talker_cpp.cpp
+++ src/my_first_pkg/src/talker_cpp.cpp
@@ -1,36 +1,52 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 
+
 #include <string>
+
 
 int main(int argc, char **argv ) {
 
+
     ros:: init(argc, argv, "talker_cpp"); //inicializando o nó
+
 
     ros::NodeHandle nh;
 
+
     ros::Rate loop_rate(10);
+
 
     ros::Publisher chatter_pub = nh.advertise<std_msgs::String>("chatter", 1000);
 
 
+
+
     int count = 0;
+
 
     while(ros::ok()) {
 
+
         std::string txt = "Ola ROS! Contagem : " + std::to_string(count);
 
+
         ROS_INFO("%s", txt.c_str());
+
 
         std_msgs::String msg;
         msg.data = txt;
         chatter_pub.publish(msg);
 
+
         ros::spinOnce();   //atualizar as informações
         loop_rate.sleep();
         ++count;
 
+
     }
+
+
 
 
     return 0;
