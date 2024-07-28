f0aca88f3018255a4f1446e5e9bff2bf907b09a2
Andre Murakami <murakkamy@gmail.com>
2024-07-28 04:58:47 -0300

    Treinamento ROS SEMEAR: 19. ROS Service Cliente em Python

--- src/my_project_py/scripts/counter.py
+++ src/my_project_py/scripts/counter.py
@@ -3,6 +3,7 @@
 import rospy
 
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty, EmptyResponse
 
 class Counter:
 
@@ -10,11 +11,11 @@ class Counter:
             self.count = 0
             self.publish_interval = 3
 
-
             self.count = 0
             self.num_sub = rospy.Subscriber("/number", Float64, self.numberCallback, queue_size = 10)
             self.count_pub = rospy.Publisher("/current_count", Float64, queue_size=10)
             self.timer_pub = rospy.Timer(rospy.Duration(self.publish_interval), self.timerCallback)
+            self.reset_srv = rospy.Service("reset_counter", Empty, self.resetSrvCallback)
 
         def numberCallback(self, msg):
             self.count = self.count + msg.data
@@ -24,6 +25,11 @@ class Counter:
             msg = Float64()
             msg.data = self.count
             self.count_pub.publish(msg)
+
+        def resetSrvCallback(self, req):
+            self.count = 0
+            rospy.loginfo("Resetando a contagem")
+            return EmptyResponse
 
 
 if __name__ == '__main__':
--- src/my_project_py/scripts/number_publisher.py
+++ src/my_project_py/scripts/number_publisher.py
@@ -2,19 +2,27 @@
 
 import rospy
 from std_msgs.msg import Float64
+from std_srvs.srv import Empty
 
 class NumberPublisher:
     def __init__(self):
         self.number = 3.1
         self.publish_interval = 1.5
+        self.reset_interval = 8
 
         self.num_pub = rospy.Publisher("/number", Float64, queue_size=10)
         self.timer_pub = rospy.Timer(rospy.Duration(self.publish_interval), self.timerCallback)
+        self.timer_reset = rospy.Timer(rospy.Duration(self.reset_interval), self.timerResetCallback)
+        self.cliente_reset = rospy.ServiceProxy("reset_counter", Empty)
 
     def timerCallback(self, event):
         msg = Float64()
         msg.data = self.number
         self.num_pub.publish(msg)
+
+    def timerResetCallback(self, event):
+        rospy.loginfo("Solicitação de reset da contagem")
+        self.cliente_reset()
 
 if __name__ == '__main__':
     try:
